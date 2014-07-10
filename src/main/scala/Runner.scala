f979390f9937017841fc128425ffd9611a113dbd
Mathieu ANCELIN <mathieu.ancelin@gmail.com>
2014-07-10 21:09:23 +0200

    Cleanup multiple hosts runner

--- src/main/scala/Runner.scala
+++ src/main/scala/Runner.scala
@@ -7,9 +7,9 @@ import server.{NodeClient, ClusterEnv, KeyValNode}
 import scala.concurrent.{Future, Await, ExecutionContext}
 import scala.concurrent.duration.Duration
 
-object Runner extends App {
+object Host1WithClient extends App {
 
-  val nbrClients = 8
+  val nbrClients = 20
   val nbrNodes = 4
   val nbrReplicates = 2
   implicit val timeout = Duration(10, TimeUnit.SECONDS)
@@ -23,17 +23,17 @@ object Runner extends App {
   val client = NodeClient(env)
 
   env.start()
-  node1.start("127.0.0.1", 7000)
-  node2.start(seedNodes = Seq("127.0.0.1:7000"))
-  node3.start(seedNodes = Seq("127.0.0.1:7000"))
-  client.start(seedNodes = Seq("127.0.0.1:7000"))
+  node1.start("192.168.1.34", 7000)
+  node2.start(seedNodes = Seq("192.168.1.34:7000"))
+  node3.start(seedNodes = Seq("192.168.1.34:7000"))
+  client.start(seedNodes = Seq("192.168.1.34:7000"))
 
-  Thread.sleep(2000)
+  Thread.sleep(20000)
 
   def scenario: Unit = {
-    for (i <- 0 to 10) {
+    for (i <- 0 to 100) {
       var seq = Seq[String]()
-      for (j <- 0 to 10) {
+      for (j <- 0 to 1000) {
         val id = IdGenerator.uuid
         seq = seq :+ id
         Await.result(client.set(id)(Json.obj("hello" -> "world", "id" -> id, "stuff1" -> IdGenerator.extendedToken(256), "stuff2" -> IdGenerator.extendedToken(256))), timeout)
@@ -56,10 +56,35 @@ object Runner extends App {
   Runtime.getRuntime.addShutdownHook(new Thread() {
     override def run(): Unit = {
       env.stop()
-      node1.stop()
-      node2.stop()
-      node3.stop()
+      node1.stop().destroy()
+      node2.stop().destroy()
+      node3.stop().destroy()
     }
   })
 
+}
+
+object Host2 extends App {
+
+  implicit val timeout = Duration(10, TimeUnit.SECONDS)
+  implicit val ec = ExecutionContext.fromExecutor(Executors.newCachedThreadPool())
+
+  val env = ClusterEnv(2)
+  val node1 = KeyValNode(s"node1", env)
+  val node2 = KeyValNode(s"node2", env)
+  val node3 = KeyValNode(s"node3", env)
+
+  env.start()
+  node1.start(seedNodes = Seq("192.168.1.34:7000"))
+  node2.start(seedNodes = Seq("192.168.1.34:7000"))
+  node3.start(seedNodes = Seq("192.168.1.34:7000"))
+
+  Runtime.getRuntime.addShutdownHook(new Thread() {
+    override def run(): Unit = {
+      env.stop()
+      node1.stop().destroy()
+      node2.stop().destroy()
+      node3.stop().destroy()
+    }
+  })
 }
